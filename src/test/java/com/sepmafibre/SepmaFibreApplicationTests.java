dc0fbfddc7a3615e162d693451f1b05edf097466
dibya <dibya@LAPTOP-3DPHTKB6>
2020-11-23 00:56:15 +0530

    Done

---
+++ src/test/java/com/sepmafibre/SepmaFibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepmafibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepmaFibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
